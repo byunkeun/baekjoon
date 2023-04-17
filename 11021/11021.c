6bfdd9df37d0253dc0de0542429795b844665fa9
byunkeun <byunkeun26@gmail.com>
2023-04-17 22:46:13 +0900

    "main"

--- 11021/11021.c
+++ 11021/11021.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int num;
+    int T;
     int result;
 
-    scanf("%d", &num);
+    scanf("%d", &T);
 
-    for( int n = 1 ; n <= num ; n++)
+    for( int n = 1 ; n <= T ; n++)
     {
-        int a,b;
+        int a, b;
         scanf("%d %d", &a, &b);
         result = a + b;
-        printf("%d\n", result);
+        printf("Case #%d: %d\n", n, result);
     }
-    
     return 0;
 }
