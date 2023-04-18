59fe147656294c9dc6286bb02a789cb785fc721c
byunkeun <byunkeun26@gmail.com>
2023-04-18 16:55:06 +0900

    "main"

---
+++ 10818/10818.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int T;
+
+    scanf("%d", &T);
+    int num[T];
+
+
+    for( int n ; n < T ; n++)
+    {
+        scanf("%d", &num[T]);
+    }
+
+    
+
+    return 0;
+}
