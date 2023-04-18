9d30e41d6ba46803f49ae6cbac5bcf24bf2b1383
byunkeun <byunkeun26@gmail.com>
2023-04-18 19:00:14 +0900

    "main"

--- 10818/10818.c
+++ 10818/10818.c
@@ -1,18 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int T;
+    unsigned int T;
 
     scanf("%d", &T);
-    int num[T];
 
+    long num[T];
 
-    for( int n ; n < T ; n++)
+    for (int n = 0; n < T; n++)
     {
-        scanf("%d", &num[T]);
+        scanf("%ld", &num[n]);
+    }
+    long max = num[0];
+    long min = num[0];
+
+    for (int k = 0; k < T; k++)
+    {
+        if (max < num[k])
+        {
+            max = num[k];
+        }
+      
+        if (min > num[k])
+        {
+            min = num[k];
+        }
+        
     }
 
-    
+    printf("%ld %ld\n", min, max);
 
     return 0;
 }
